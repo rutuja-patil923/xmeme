4dbc5f0fbb96181c2d11c7cbf6e030367b0a3d6d
rutuja-patil107 <68247202+rutuja-patil107@users.noreply.github.com>
2021-02-14 21:04:10 +0530

    updated

--- install.sh
+++ install.sh
@@ -20,5 +20,6 @@ sudo systemctl enable mongod
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+sudo apt update
 
 sudo fuser -k 8080/tcp
